0df40bdf3560bb7abbea2dc3715fa3bc2c930c50
Peter Malina <peter.malina@flowup.cz>
2018-06-18 13:35:24 +0200

    feat(ui/libs): added material

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+  MatToolbarModule, MatCardModule, MatAutocompleteModule,
+  MatBadgeModule, MatButtonModule, MatIconModule
+} from '@angular/material'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    MatToolbarModule, MatCardModule, MatAutocompleteModule,
+    MatBadgeModule, MatButtonModule, MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
