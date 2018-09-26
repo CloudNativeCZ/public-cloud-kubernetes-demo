48cb1ecf0f0340929bb57defbfa6a81ef8003551
Robert Zahradníček <rzahradnicek@gmail.com>
2018-09-26 12:22:31 +0200

    Add tracing to main server

--- backend/cmd/server.go
+++ backend/cmd/server.go
@@ -10,16 +10,21 @@ import (
 	log "github.com/golang/glog"
 
 	"github.com/cloudnativecz/public-cloud-kubernetes-demo/backend/api"
-	"github.com/cloudnativecz/public-cloud-kubernetes-demo/backend/pkg"
+  "github.com/cloudnativecz/public-cloud-kubernetes-demo/backend/pkg"
+  "github.com/cloudnativecz/public-cloud-kubernetes-demo/backend/pkg/tracing"
 	"github.com/go-redis/redis"
 
 	"github.com/emicklei/go-restful"
+
+  // Tracing dependencies
+  "github.com/opentracing/opentracing-go"
 )
 
 type AppOptions struct {
 	backingStoreOptions *redis.Options
-	listenHost          string
-	listenPort          int
+	listenHost                string
+	listenPort                int
+  tracingClientHostPort     string
 }
 
 type App struct {
@@ -55,6 +60,12 @@ func (app *App) parseEnvVars() {
 
 	app.options.backingStoreOptions.Addr = fmt.Sprintf("%s:%s", host, port)
 	app.options.backingStoreOptions.DB = db
+
+  // Pass tracing client backend via environment variables
+  tracingClientHost := os.Getenv("TRACING_CLIENT_HOST")
+  tracingClientPort := os.Getenv("TRACING_CLIENT_PORT")
+
+  app.options.tracingClientHostPort = fmt.Sprintf("%s:%s", tracingClientHost, tracingClientPort)
 }
 
 func (app *App) initiateBackingStore() {
@@ -74,28 +85,50 @@ func (app *App) serve(container *restful.Container) {
 }
 
 func main() {
-	app := newApp()
-	app.addFlags()
-	app.parseFlags()
-	app.parseEnvVars()
+  app := newApp()
+  app.addFlags()
+  app.parseFlags()
+  app.parseEnvVars()
 
-	app.initiateBackingStore()
-	wsContainer := restful.NewContainer()
+  tracer := tracing.Init("server", app.options.tracingClientHostPort)
 
-	// Add container filter to enable CORS
-	cors := restful.CrossOriginResourceSharing{
-		ExposeHeaders:  []string{"X-My-Header"},
-		AllowedHeaders: []string{"Content-Type", "Accept"},
-		AllowedMethods: []string{"GET", "PUT"},
-		CookiesAllowed: false,
-		Container:      wsContainer}
-	wsContainer.Filter(cors.Filter)
+  parent := tracer.StartSpan("Boostrap")
 
-	// Add container filter to respond to OPTIONS
-	wsContainer.Filter(wsContainer.OPTIONSFilter)
+  child = tracer.StartSpan("BackingStoreBoostrap", opentracing.ChildOf(parent.Context()))
+  app.initiateBackingStore()
+  child.LogEvent("Backing store initiated")
+  child.Finish()
 
-	questionsResource := api.NewQuestionsResource(app.backingStore)
-	questionsResource.Register(wsContainer)
+  child = tracer.StartSpan("WSContainerBoostrap", opentracing.ChildOf(parent.Context()))
+  wsContainer := restful.NewContainer()
+  child.LogEvent("WS Container created")
 
-	app.serve(wsContainer)
+  // Add container filter to enable CORS
+  cors := restful.CrossOriginResourceSharing{
+    ExposeHeaders:  []string{"X-My-Header"},
+    AllowedHeaders: []string{"Content-Type", "Accept"},
+    AllowedMethods: []string{"GET", "PUT"},
+    CookiesAllowed: false,
+    Container:      wsContainer}
+  wsContainer.Filter(cors.Filter)
+  child.LogEvent("WS Container CORS Filter created")
+
+  // Add container filter to respond to OPTIONS
+  wsContainer.Filter(wsContainer.OPTIONSFilter)
+  child.LogEvent("WS Container CORS OPTIONS Filter added")
+
+  child.LogEvent("App boot finished")
+  child.Finish()
+
+  child = tracer.StartSpan("QuestionsApiBoostrap", opentracing.ChildOf(parent.Context()))
+  questionsResource := api.NewQuestionsResource(app.backingStore, app.options.tracingClientHostPort)
+  child.LogEvent(fmt.Sprintf("Question Resource started with %v", app.backingStore))
+  questionsResource.Register(wsContainer)
+
+  child.LogEvent(fmt.Sprintf("Question Resource registered with WS Container %v", wsContainer))
+  child.LogEvent("Starting serving content")
+  child.Finish()
+  parent.Finish()
+
+  app.serve(wsContainer)
 }
